e6b19743ee479c1c623627dd9a6ddc637b39bdfa
fators <fators@bergen.org>
2024-06-05 12:21:30 -0400

    Static caching

--- src/main/java/com/forsun/fifa/MatchController.java
+++ src/main/java/com/forsun/fifa/MatchController.java
@@ -1,11 +1,14 @@
 package com.forsun.fifa;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -13,9 +16,11 @@ import java.util.List;
 public class MatchController {
     @Autowired
     private MatchService mservice;
+    private String Files="static.json";
 
     @GetMapping("/display")
     public List<Matches> getAllMatch(){
+        saveMatchesToFile(mservice.findAllMatches());
         return  mservice.findAllMatches();
 
     }
@@ -38,6 +43,15 @@ public class MatchController {
     @GetMapping("/overall-winner")
     public String getOverallWinner(){
         return "";
+    }
+    // Method to save matches to a JSON file using FileWriter
+    private void saveMatchesToFile(List<Matches> matches) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try (FileWriter fileWriter = new FileWriter(Files)) {
+            objectMapper.writeValue(fileWriter, matches);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
